61211977f5238a68be5973a82d6614c61622c666
ZEFRAX <rinops@live.no>
2017-05-12 10:23:34 +0200

    Fixed navbar overlapping with h1 title

--- home.php
+++ home.php
@@ -13,6 +13,9 @@
  $userRow=mysql_fetch_array($res);
 
 ?>
+<style media="screen">
+  .top-buffer { margin-top: 30px;}
+</style>
 <!DOCTYPE html>
 <html>
 <?php
@@ -22,12 +25,11 @@ include'includes/navbar.php'; ?>
 <body>
  <div id="wrapper">
 
- <div class="container">
+ <div class="container top-buffer">
 
-     <div class="page-header">
+     <div class="page-header ">
      <h3>Coding Cage - Programming Blog</h3>
      </div>
-
         <div class="row">
         <div class="col-lg-12">
         <h1>Focuses on PHP, MySQL, Ajax, jQuery, Web Design and more...</h1>
