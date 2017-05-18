d22635b356267c7ad3c07bc1aef27ef224e7272b
ZEFRAX <rinops@live.no>
2017-05-18 11:19:49 +0200

    Added a form in kontrollpanel.php for adding products

--- kontrollpanel.php
+++ kontrollpanel.php
@@ -17,25 +17,217 @@ include'includes/navbar.php';
 if ($userRow['userStat'] != '1') {
   header("Location: index.php");}
   //Edit html under here  vvvv
+
+
+ $error = false;
+
+ if ( isset($_POST['btn-signup']) ) {
+
+  // clean user inputs to prevent sql injections
+  $productName = trim($_POST['productName']);
+  $productName = strip_tags($productName);
+  $productName = htmlspecialchars($productName);
+
+  $productDesc = trim($_POST['productDesc']);
+  $productDesc = strip_tags($productDesc);
+  $productDesc = htmlspecialchars($productDesc);
+
+  $productPrice = trim($_POST['productPrice']);
+  $productPrice = strip_tags($productPrice);
+  $productPrice = htmlspecialchars($productPrice);
+
+  $productStock = trim($_POST['productStock']);
+  $productStock = strip_tags($productStock);
+  $productStock = htmlspecialchars($productStock);
+
+  $productImage = trim($_POST['productImage']);
+  $productImage = strip_tags($productImage);
+  $productImage = htmlspecialchars($productImage);
+
+
+  // basic name validation
+  if (empty($productName)) {
+   $error = true;
+   $productNameError = "Please enter your full productName.";
+  } else if (strlen($productName) < 3) {
+   $error = true;
+   $productNameError = "Name must have atleat 3 characters.";
+ } else if (!preg_match("/^[a-zA-Z ÆØÅæøå]+$/",$productName)) {
+   $error = true;
+   $productNameError = "productName must contain alphabets and space.";
+  }
+
+  //basic email validation
+  if (empty($productDesc)) {
+   $error = true;
+   $productDescError = "Please enter valid productDesc address.";
+  }
+
+  // basic productPrice validation
+  if (empty($productPrice)) {
+   $error = true;
+   $productPriceError = "Please enter your full productPrice.";
+ } else if (!preg_match("/^[0-9]+$/",$productPrice)) {
+   $error = true;
+   $productPriceError = "productPrice must contain alphabets and space.";
+  }
+
+  // productStock validation
+  if (empty($productStock)){
+   $error = true;
+   $productStockError = "Skriv inn productStock";
+ } else if(strlen($productStock) < 1) {
+   $error = true;
+   $productStockError = "productStock må minst ha 6 bokstaver.";
+ }
+
+ // productImage validation
+ if (empty($productImage)){
+  $error = true;
+  $productImageError = "Skriv inn productImage";
+} else if(strlen($productImage) < 1) {
+  $error = true;
+  $productImageError = "productImage må minst ha 6 bokstaver.";
+}
+// productStock validation
+if (empty($productRating)){
+ $error = true;
+ $productRatingError = "Skriv inn productRating";
+} else if(strlen($productRating) < 1) {
+ $error = true;
+ $productRatingError = "productRating må minst ha 6 bokstaver.";
+}
+
+  // if there's no error, continue to signup
+  if( !$error ) {
+
+   $query = "INSERT INTO users(productName,productDesc,productPrice, productStock, productImage, productRating) VALUES('$productName','$productDesc','$productPrice','$productStock','$productImage','$productRating')";
+   $res = mysql_query($query);
+
+   if ($res) {
+    $errTyp = "success";
+    $errMSG = "Successfully registered, you may login now";
+    unset($productName);
+    unset($productDesc);
+    unset($productPrice);
+    unset($productStock);
+    unset($productImage);
+    unset($productRating);
+    header("Location: login.php?success");
+   } else {
+    $errTyp = "danger";
+    $errMSG = "Something went wrong, try again later...";
+   }
+
+  }
+
+
+ }
 ?>
-
-
-
-<!-- HTML START-->
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registrering</title>
+</head>
 <body>
- <div id="wrapper">
-   <div class="container top-buffer-30">
-     <div class="page-header ">
-       <h3>Standard Administrator side</h3>
-     </div>
-     <div class="row">
-       <div class="col-lg-12">
-         <h1>Dette er en standard Administrator side, personer uten userStat satt til 1 vill ikke kunne se denne siden.</h1>
-       </div>
-     </div>
+
+<div class="container top-buffer-30">
+
+ <div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+
+     <div class="col-md-12">
+
+         <div class="form-group">
+             <h2 class="">Legg til varer</h2>
+            </div>
+
+         <div class="form-group">
+             <hr />
+            </div>
+
+            <?php
+   if ( isset($errMSG) ) {
+
+    ?>
+    <div class="form-group ">
+             <div class="alert alert-<?php echo ($errTyp=="success") ? "success" : $errTyp; ?>">
+    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+             </div>
+                <?php
+   }
+   ?>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+             <input type="text" name="productName" class="form-control" placeholder="Varenavn" maxlength="50" value="<?php echo $productName ?>" />
+                </div>
+                <span class="text-danger"><?php echo $productNameError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+             <input type="text" name="productDesc" class="form-control" placeholder="Beskrivelse" maxlength="40" value="<?php echo $productDesc ?>" />
+                </div>
+                <span class="text-danger"><?php echo $productDescError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-earphone"></span></span>
+             <input type="text" name="productPrice" class="form-control" placeholder="Pris" maxlength="8" value="<?php echo $productPrice ?>" />
+                </div>
+                <span class="text-danger"><?php echo $productPriceError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="text" name="productStock" class="form-control" placeholder="Stock" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $productStockError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="text" name="productImage" class="form-control" placeholder="productImage link" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $productImageError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="text" name="productRating" class="form-control" placeholder="Rating number" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $productRatingError; ?></span>
+            </div>
+
+
+
+
+            <div class="form-group">
+             <hr />
+            </div>
+
+            <div class="form-group">
+             <button type="submit" class="btn btn-block btn-primary" name="btn-signup">Lagre</button>
+            </div>
+
+            <div class="form-group">
+             <hr />
+            </div>
+        </div>
+
+    </form>
     </div>
-  </div>
+
+</div>
+
 </body>
 </html>
-<!-- HTML END-->
 <?php ob_end_flush(); ?>
