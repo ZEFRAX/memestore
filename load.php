93b39ce004aeef9e9931f2b20323a49790351c9b
Pinchcliffe <haakonkverneng@gmail.com>
2017-05-19 13:32:20 +0200

    Load.php - Dynamisk rating system, oppdatert.

--- load.php
+++ load.php
@@ -1,27 +1,26 @@
-<?php
-include 'includes/dbConnect.php';
+<?php include'includes/dbConnect.php';
 
-$sql    = "SELECT productImage, productPrice, productName, productDesc, productTime, productRating FROM products";
+$sql = "SELECT productImage, productPrice, productName, productDesc, productTime, productRating FROM products";
 $result = mysql_query($sql);
 if (mysql_num_rows($result) > 0) {
     // output data of each row
     $i = 0;
     $e = 0;
-    while ($row = mysql_fetch_assoc($result)) {
+    while($row = mysql_fetch_assoc($result)) {
         $i++;
         $e++;
 
-        $element      = "<span class='glyphicon glyphicon-star'></span>";
+        $element = "<span class='glyphicon glyphicon-star'></span>";
         $emptyElement = "<span class='glyphicon glyphicon-star-empty'></span>";
-        $emptyCount   = 5;
-        $count        = $row["productRating"];
+        $emptyCount = 5;
+        $count = $row["productRating"];
 
-        $additionalClass  = ($i % 3) == 0 ? " <div class='row'>" : "";
+        $additionalClass = ($i % 3) == 0 ? " <div class='row'>" : "";
         $additionalClass2 = ($e % 3) == 0 ? " </div>" : "";
-        echo "" . $additionalClass . "<div class=' col-xs-4 col-sm-4 col-lg-4 col-md-4'>
-       <div class='thumbnail cube'> <img src='" . $row["productImage"] . "' alt=''>
+        echo "".$additionalClass."<div class=' col-xs-4 col-sm-4 col-lg-4 col-md-4'>
+       <div class='thumbnail cube'> <img src='". $row["productImage"] ."' alt=''>
        <div class='caption'>
-       <h4 class='pull-right'>" . $row["productPrice"] . ",-</h4>
+       <h4 class='pull-right'>" . $row["productPrice"].",-</h4>
        <h4><a href='#'>" . $row["productName"] . "</a> </h4>
        <p class='text'>" . $row["productDesc"] . "</p>
        </div>
@@ -86,18 +85,18 @@ if (mysql_num_rows($result) > 0) {
             for ($a = 0; $a < $count; $a++) {
                 echo $element;
             }
-        } else {
+        }
+        else {
             for ($a = 0; $a < $emptyCount; $a++) {
                 echo $emptyElement;
             }
         }*/
 
-        echo "</p></div></div></div>" . $additionalClass2 . "";
+        echo "</p></div></div></div>".$additionalClass2."";
     }
 
 } else {
     echo "0 results";
 
 }
-mysql_close($conn);
-?>
+mysql_close($conn);?>
