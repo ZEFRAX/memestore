7a4615047520354e39f4ce65b4043b715dc7b539
ZEFRAX <rinops@live.no>
2017-05-19 10:11:37 +0200

    reverted mp4 and webm

--- kontrollpanel.php
+++ kontrollpanel.php
@@ -124,7 +124,7 @@ if ($_FILES["fileToUpload"]["size"] > 10000000) {
   $error = true;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "mp4" && $imageFileType != "webm"
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
   $productImageColor = "text-danger";
   $productImageError = "Beklager bare JPG, JPEG, PNG, MP4, WEBM & GIF filer er tillat.";
